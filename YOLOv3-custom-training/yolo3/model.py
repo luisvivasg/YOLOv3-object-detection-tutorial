67eb431cb07f9b4fab47e3d898cce3c9751a92e6
luisvivasg <63152802+luisvivasg@users.noreply.github.com>
2020-07-17 12:39:03 +0200

    Update model.py

--- YOLOv3-custom-training/yolo3/model.py
+++ YOLOv3-custom-training/yolo3/model.py
@@ -4,10 +4,10 @@ from functools import wraps
 
 import numpy as np
 import tensorflow as tf
-from keras import backend as K
-from keras.layers import Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, MaxPooling2D
-from keras.layers.advanced_activations import LeakyReLU
-from keras.layers.normalization import BatchNormalization
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.layers import Conv2D, Add, ZeroPadding2D, UpSampling2D, Concatenate, MaxPooling2D
+from tensorflow.python.layers.advanced_activations import LeakyReLU
+from tensorflow.python.layers.normalization import BatchNormalization
 from keras.models import Model
 from keras.regularizers import l2
 
